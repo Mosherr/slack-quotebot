aece84ea3e79216ba040aaafcf2eba242be64fe8
mosherr <danscherr@gmail.com>
2017-06-11 15:19:32 -0700

    vendor

--- config.go
+++ config.go
@@ -52,11 +52,17 @@ var (
 	// requests to the slash command come from Slack. It is provided for
 	// you by Slack when you create the Slash command as a custom
 	// integration. https://my.slack.com/services/new/slash-commands
-	token string = "xxxxxx"
+	token string = "QtFJq3lpxo491tdjPgJiaKRI"
 
-	MongoDBHosts string = "xxxxxx:xxxxx"
-	AuthUserName string = "xxxxxx"
-	AuthPassword string = "xxxxxx"
+	MongoDBHosts string = "ds145299.mlab.com:45299"
+	AuthUserName string = "quotemaster"
+	AuthPassword string = "md3EKiXFZ2TA"
+
+	//token string = "xxxxxx"
+	//
+	//MongoDBHosts string = "xxxxxx:xxxxx"
+	//AuthUserName string = "xxxxxx"
+	//AuthPassword string = "xxxxxx"
 
 	DB QuoteDatabase
 
